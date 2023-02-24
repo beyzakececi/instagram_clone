0f87ff51d515fb66020e46b303d0608ba755d26c
beyzakececi <beyzakececi.35@gmail.com>
2023-02-24 15:06:57 +0300

    Fix Gradle task assembleDebug failed with exit code 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 import 'responsive/responsive_layout_screen.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
